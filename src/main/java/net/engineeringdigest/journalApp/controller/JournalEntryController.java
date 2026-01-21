7920eb752ae33803ee86ec204e967b5281489c07
Tanveer Singh <tanveerbadhan@gmail.com>
2026-01-20 21:20:50 -0500

    First Commit

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,9 @@
+package net.engineeringdigest.journalApp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+}
