4776cd546d973acd58721b9327190c6c26961a66
Tanveer Singh <tanveerbadhan@gmail.com>
2026-01-20 23:18:28 -0500

    Attached mongodb with springboot and written crud

--- src/main/java/com/tanveer/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/tanveer/journalApp/controller/JournalEntryController.java
@@ -2,11 +2,13 @@ package com.tanveer.journalApp.controller;
 
 import com.tanveer.journalApp.entity.JournalEntry;
 import com.tanveer.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/journal")
@@ -15,9 +17,37 @@ public class JournalEntryController {
     @Autowired
     private JournalEntryService journalEntryService;
 
-    @PostMapping
-    public boolean saveEntry(@RequestBody JournalEntry myEntry){
-        journalEntryService.saveEntry(myEntry);
-        return true;
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return journalEntryService.getAll();
     }
+
+    @GetMapping("/id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myId){
+        return journalEntryService.findById(myId).orElse(null);
+    }
+
+    @PostMapping
+    public JournalEntry createEntry(@RequestBody JournalEntry myEntry){
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
+        return myEntry;
+    }
+
+    @PutMapping("/id/{myId}")
+    public JournalEntry updateEntry(@PathVariable ObjectId myId, @RequestBody JournalEntry updatedEntry){
+        JournalEntry oldEntry = journalEntryService.findById(myId).orElse(null);
+        if(oldEntry != null){
+            oldEntry.setTitle(updatedEntry.getTitle() != null && !updatedEntry.getTitle().equals("") ? updatedEntry.getTitle() : oldEntry.getTitle());
+            oldEntry.setContent(updatedEntry.getContent() != null && !updatedEntry.getContent().equals("") ? updatedEntry.getContent() : oldEntry.getContent());
+        }
+        journalEntryService.saveEntry(oldEntry);
+        return oldEntry;
+    }
+
+    @DeleteMapping("/id/{myId}")
+    public void deleteJournalEntryById(@PathVariable ObjectId myId){
+        journalEntryService.deleteById(myId);
+    }
+
 }
--- src/main/java/com/tanveer/journalApp/entity/JournalEntry.java
+++ src/main/java/com/tanveer/journalApp/entity/JournalEntry.java
@@ -1,31 +1,33 @@
 package com.tanveer.journalApp.entity;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
 public class JournalEntry {
 
     @Id
-    private String id;
+    private ObjectId id;
     private String title;
     private String content;
-    private Date date;
+    private LocalDateTime date;
 
-    public String getId() {
+    public ObjectId getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(ObjectId id) {
         this.id = id;
     }
 
-    public Date getDate() {
+    public LocalDateTime getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(LocalDateTime date) {
         this.date = date;
     }
 
